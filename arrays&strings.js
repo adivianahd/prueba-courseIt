06955ebfc9a4a6dedf106233b7904ec904292af5
adivianahd <adivianahd07@gmail.com>
2020-01-13 20:27:58 -0300

    clases

--- arrays&strings.js
+++ arrays&strings.js
@@ -15,22 +15,24 @@ function suma(a, b) {
 }*/
 
 class Counter {
-    constructor() {
-        this.value = 0
+    constructor(startValue) {
+        this.value = startValue
     }
+
     suma() {
         this.value = this.value + 1
     }
+
     resta() {
         this.value = this.value - 1
     }
 }
-const counter = new Counter()
-console.log(counter.value)
+
+const counter = new Counter(0)
+console.log(counter.value) //0
 counter.suma()
 counter.suma()
-console.log(counter.value)
 counter.suma()
-console.log(counter.value)
-counter.suma()
-console.log(counter.value)
+console.log(counter.value) //3
+counter.resta()
+console.log(counter.value) //2
